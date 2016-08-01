8627273a08f6a9b6fc9fb8e9facfc14e246d3597
SynWade <nebojsa.orescanin@gmail.com>
2016-07-31 21:28:20 -0400

    Added menu, about us, event booking, and contact us still needs work

--- megacone.ca/php/submit.php
+++ megacone.ca/php/submit.php
@@ -5,12 +5,15 @@
 	$email = $_POST["email"];
 	$eventType = $_POST["eventType"];
 	$numberPeople = $_POST["numberPeople"];
+	$iceCreamTruck = $_POST["iceCreamTruck"];
+	$lemonadeTrailer = $_POST["lemonadeTrailer"];
+	$crepeTruck = $_POST["crepeTruck"];
 	$location = $_POST["location"];
 	$date = $_POST["date"];
 	$time = $_POST["time"];
 	$from = $firstName . ", " . $lastName; // sender
     $subject = "Event Booking";
-    $message = "First Name: " . $firstName . "\r\n Last Name: " . $lastName . "\r\n Phone Number: " . $phoneNumber . "\r\n Email: " . $email . "\r\n Event Type: " . $eventType . "\r\n Number of People: " . $numberPeople . "\r\n Location: " . $location . "\r\n Date: " . $date . "\r\n Time: " . $time;
+    $message = "First Name: " . $firstName . "\r\n Last Name: " . $lastName . "\r\n Phone Number: " . $phoneNumber . "\r\n Email: " . $email . "\r\n Event Type: " . $eventType . "\r\n Number of People: " . $numberPeople . "\r\n Include Ice Cream Truck: " . $iceCreamTruck . "\r\n Include Lemonade Trailer: " . $lemonadeTrailer . "\r\n Include Crepe Truck: " . $crepeTruck . "\r\n Location: " . $location . "\r\n Date: " . $date . "\r\n Time: " . $time;
     // message lines should not exceed 70 characters (PHP rule), so wrap it
 	$message = wordwrap($message, 70);
     // send mail
